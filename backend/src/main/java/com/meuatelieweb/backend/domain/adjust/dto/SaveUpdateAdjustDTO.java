5f96f9bb3504fa47fbf06c7714738f91fb6a5ac4
Isadora <isah3027@gmail.com>
2024-01-17 10:57:45 -0300

    tests(AdjustService): implement unit tests to addAdjust method

--- backend/src/main/java/com/meuatelieweb/backend/domain/adjust/dto/SaveUpdateAdjustDTO.java
+++ backend/src/main/java/com/meuatelieweb/backend/domain/adjust/dto/SaveUpdateAdjustDTO.java
@@ -15,8 +15,7 @@ public class SaveUpdateAdjustDTO {
     @NotBlank(message = "The given name cannot be empty")
     private String name;
 
-    @NotNull(message = "The given cost cannot be empty")
     @Positive(message = "The given cost must be higher than 0")
-    @Digits(integer = 5, fraction = 2, message = "The cost must have up to 5 digits in total, with 2 decimals")
+    @Digits(integer = 4, fraction = 2, message = "The cost must have up to 4 digits in total, with 2 decimals")
     private Double cost;
 }
--- backend/src/test/java/com/meuatelieweb/backend/domain/adjust/AdjustServiceTest.java
+++ backend/src/test/java/com/meuatelieweb/backend/domain/adjust/AdjustServiceTest.java
@@ -1,6 +1,7 @@
 package com.meuatelieweb.backend.domain.adjust;
 
 import com.meuatelieweb.backend.domain.adjust.dto.AdjustDTO;
+import com.meuatelieweb.backend.domain.adjust.dto.SaveUpdateAdjustDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -10,10 +11,12 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.List;
 import java.util.Optional;
@@ -22,6 +25,7 @@ import java.util.UUID;
 import static com.meuatelieweb.backend.domain.adjust.AdjustCreator.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Tests for Adjust Service")
@@ -128,6 +132,77 @@ class AdjustServiceTest {
         void findById_ThrowsEntityNotFoundException_WhenIdAdjustIsNull() {
             assertThrows(EntityNotFoundException.class,
                     () -> adjustService.findById(null));
+        }
+    }
+
+    @DisplayName("Test addAdjust method")
+    @Nested
+    class AddAdjustTests {
+
+        private void mockRepositorySave(){
+            BDDMockito.when(adjustRepositoryMock.save(any(Adjust.class)))
+                    .thenReturn(createValidAdjust());
+        }
+
+        @Test
+        @DisplayName("addAdjust returns adjust when successful")
+        void addAdjust_ReturnsAdjust_WhenSuccessful() {
+            AdjustDTO adjustDTO = createValidAdjustDTO(createValidAdjust().getId());
+
+            this.mockRepositorySave();
+
+            mockConverterToAdjustDTO(adjustDTO);
+
+            AdjustDTO adjustSaved = adjustService.addAdjust(createValidSaveUpdateAdjustDTO());
+
+            assertNotNull(adjustSaved);
+            assertEquals(adjustDTO, adjustSaved);
+        }
+
+        @Test
+        @DisplayName("addAdjust throws IllegalArgumentException when adjust is null")
+        void addAdjust_ThrowsIllegalArgumentException_WhenAdjustIsNull() {
+            assertThrows(IllegalArgumentException.class,
+                    () -> adjustService.addAdjust(null));
+        }
+
+        @Test
+        @DisplayName("addAdjust throws IllegalArgumentException when adjust name is null")
+        void addAdjust_ThrowsIllegalArgumentException_WhenAdjustNameIsNull() {
+            SaveUpdateAdjustDTO saveUpdateAdjustDTO = createValidSaveUpdateAdjustDTO();
+            saveUpdateAdjustDTO.setName(null);
+
+            assertThrows(IllegalArgumentException.class,
+                    () -> adjustService.addAdjust(saveUpdateAdjustDTO));
+        }
+
+        @Test
+        @DisplayName("addAdjust throws DuplicateKeyException when adjust name already exists")
+        void addAdjust_ThrowsDuplicateKeyException_WhenAdjustNameAlreadyExists() {
+            BDDMockito.when(adjustRepositoryMock.existsByName(anyString())).thenReturn(true);
+
+            assertThrows(DuplicateKeyException.class,
+                    () -> adjustService.addAdjust(createValidSaveUpdateAdjustDTO()));
+        }
+
+        @Test
+        @DisplayName("addAdjust throws IllegalArgumentException when adjust cost is null")
+        void addAdjust_ThrowsIllegalArgumentException_WhenAdjustCostIsNull() {
+            SaveUpdateAdjustDTO saveUpdateAdjustDTO = createValidSaveUpdateAdjustDTO();
+            saveUpdateAdjustDTO.setCost(null);
+
+            assertThrows(IllegalArgumentException.class,
+                    () -> adjustService.addAdjust(saveUpdateAdjustDTO));
+        }
+
+        @Test
+        @DisplayName("addAdjust throws IllegalArgumentException when adjust cost is lesser than 0.01 cent")
+        void addAdjust_ThrowsIllegalArgumentException_WhenAdjustCostIsLesserThan1Cent() {
+            SaveUpdateAdjustDTO saveUpdateAdjustDTO = createValidSaveUpdateAdjustDTO();
+            saveUpdateAdjustDTO.setCost(0.0);
+
+            assertThrows(IllegalArgumentException.class,
+                    () -> adjustService.addAdjust(saveUpdateAdjustDTO));
         }
     }
 }
