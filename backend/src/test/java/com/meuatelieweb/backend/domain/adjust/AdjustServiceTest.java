fd44db9a6334ed77ad8513f5d8dc4ce976263533
Isadora <isah3027@gmail.com>
2024-01-17 10:28:35 -0300

    tests(AdjustService): implement unit tests to findById method

--- backend/src/test/java/com/meuatelieweb/backend/domain/adjust/AdjustServiceTest.java
+++ backend/src/test/java/com/meuatelieweb/backend/domain/adjust/AdjustServiceTest.java
@@ -1,6 +1,7 @@
 package com.meuatelieweb.backend.domain.adjust;
 
 import com.meuatelieweb.backend.domain.adjust.dto.AdjustDTO;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static com.meuatelieweb.backend.domain.adjust.AdjustCreator.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -80,6 +83,51 @@ class AdjustServiceTest {
 
             assertNotNull(adjustList);
             assertTrue(adjustList.isEmpty());
+        }
+    }
+
+    @DisplayName("Test findById method")
+    @Nested
+    class FindByIdTests {
+
+        private void mockRepositoryFindById(){
+            BDDMockito.when(adjustRepositoryMock.findById(any(UUID.class)))
+                    .thenReturn(Optional.of(createValidAdjust()));
+        }
+
+        @Test
+        @DisplayName("findById returns adjust when successful")
+        void findById_ReturnsAdjust_WhenSuccessful() {
+            AdjustDTO adjustDTO = createValidAdjustDTO(UUID.randomUUID());
+
+            this.mockRepositoryFindById();
+
+            mockConverterToAdjustDTO(adjustDTO);
+
+            AdjustDTO adjustFound = adjustService.findById(UUID.randomUUID());
+
+            assertNotNull(adjustFound);
+            assertEquals(adjustDTO.getId(), adjustFound.getId());
+            assertEquals(adjustDTO.getName(), adjustFound.getName());
+            assertEquals(adjustDTO.getCost(), adjustFound.getCost());
+            assertEquals(adjustDTO.getIsActive(), adjustFound.getIsActive());
+        }
+
+        @Test
+        @DisplayName("findById throws EntityNotFoundException when adjust is not found")
+        void findById_ThrowsEntityNotFoundException_WhenAdjustIsNotFound() {
+            BDDMockito.when(adjustRepositoryMock.findById(any(UUID.class)))
+                    .thenReturn(Optional.empty());
+
+            assertThrows(EntityNotFoundException.class,
+                    () -> adjustService.findById(UUID.randomUUID()));
+        }
+
+        @Test
+        @DisplayName("findById throws EntityNotFoundException when id adjust is null")
+        void findById_ThrowsEntityNotFoundException_WhenIdAdjustIsNull() {
+            assertThrows(EntityNotFoundException.class,
+                    () -> adjustService.findById(null));
         }
     }
 }
