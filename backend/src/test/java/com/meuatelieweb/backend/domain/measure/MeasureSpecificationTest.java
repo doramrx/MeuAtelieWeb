2eea7aed4f1656d919704796a71b9f29c5be9451
Isadora <isah3027@gmail.com>
2024-01-17 10:05:46 -0300

    refactor(MeasureSpecification): use notation @BeforeAll to setUp method

--- backend/src/test/java/com/meuatelieweb/backend/domain/measure/MeasureSpecificationTest.java
+++ backend/src/test/java/com/meuatelieweb/backend/domain/measure/MeasureSpecificationTest.java
@@ -1,5 +1,6 @@
 package com.meuatelieweb.backend.domain.measure;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -26,8 +27,11 @@ class MeasureSpecificationTest {
     @InjectMocks
     private MeasureSpecification measureSpecification;
 
-    public List<Measure> populateDatabase() {
-        List<Measure> measures = List.of(
+    private List<Measure> measures;
+
+    @BeforeEach
+    public void setUpDatabase() {
+        this.measures = List.of(
                 Measure.builder()
                         .id(UUID.randomUUID())
                         .name("Medida Cintura")
@@ -45,46 +49,41 @@ class MeasureSpecificationTest {
                         .build()
         );
 
-        measures.forEach(measureRepository::save);
-        return measures;
+        this.measures.forEach(measureRepository::save);
     }
 
     @Test
     @DisplayName("applyFilter returns page of measures filtered by name when successful")
     void applyFilter_ReturnsPageOfMeasuresFilteredByName_WhenSuccessful() {
 
-        List<Measure> measures = this.populateDatabase();
-
         Specification<Measure> specification = MeasureSpecification
                 .applyFilter("Medida", null);
 
-        PageImpl<Measure> page = new PageImpl<>(measures, Pageable.ofSize(20), 20);
+        PageImpl<Measure> page = new PageImpl<>(this.measures, Pageable.ofSize(20), 20);
 
         List<Measure> result = measureRepository.findAll(specification, page.getPageable()).toList();
 
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertEquals(2, result.size());
-        assertTrue(result.contains(measures.get(0)));
-        assertTrue(result.contains(measures.get(1)));
+        assertTrue(result.contains(this.measures.get(0)));
+        assertTrue(result.contains(this.measures.get(1)));
     }
 
     @Test
     @DisplayName("applyFilter returns page of measures filtered by isActive when successful")
     void applyFilter_ReturnsPageOfMeasuresFilteredByIsActive_WhenSuccessful() {
 
-        List<Measure> measures = this.populateDatabase();
-
         Specification<Measure> specification = MeasureSpecification
                 .applyFilter(null, false);
 
-        PageImpl<Measure> page = new PageImpl<>(measures, Pageable.ofSize(20), 20);
+        PageImpl<Measure> page = new PageImpl<>(this.measures, Pageable.ofSize(20), 20);
 
         List<Measure> result = measureRepository.findAll(specification, page.getPageable()).toList();
 
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertEquals(1, result.size());
-        assertTrue(result.contains(measures.get(2)));
+        assertTrue(result.contains(this.measures.get(2)));
     }
 }
