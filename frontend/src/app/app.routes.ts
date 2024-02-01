6b695ad9f7146c79aabd63ef74ef394573e0855f
Isadora <isah3027@gmail.com>
2024-01-31 22:21:01 -0300

    feat(wip): create customers page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CustomersComponent } from './features/customers/customers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'customers',
+    component: CustomersComponent
+  }
+];
--- frontend/src/app/features/customers/customers.component.ts
+++ frontend/src/app/features/customers/customers.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
 export class CustomersComponent {
-
+  public tse: string = "Teste";
 }
