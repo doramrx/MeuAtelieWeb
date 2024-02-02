7a72404c426b74ec7801b75687636e92b297be14
Isadora <isah3027@gmail.com>
2024-02-01 22:35:21 -0300

    feat: create header component

--- frontend/src/app/features/customers/customers.component.ts
+++ frontend/src/app/features/customers/customers.component.ts
@@ -5,13 +5,34 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
+  imports: [CommonModule, RouterLink],
   templateUrl: './customers.component.html',
-  styleUrl: './customers.component.css'
+  styleUrl: './customers.component.css',
 })
 export class CustomersComponent {
-  public tse: string = "Teste";
+  private _selectedMenuOption: MenuOptions;
+  private _isHamburgerMenuExpanded: boolean;
+
+  constructor() {
+    this._selectedMenuOption = 'ORDERS';
+    this._isHamburgerMenuExpanded = false;
+  }
+
+  get selectedMenuOption() {
+    return this._selectedMenuOption;
+  }
+
+  get isHamburgerMenuExpanded() {
+    return this._isHamburgerMenuExpanded;
+  }
+
+  changeSelectedMenuOption(menuOption: MenuOptions) {
+    this._selectedMenuOption = menuOption;
+  }
+
+  toggleHamburguerMenu() {
+    this._isHamburgerMenuExpanded = !this._isHamburgerMenuExpanded;
+  }
 }
+
+type MenuOptions = 'ORDERS' | 'CUSTOMERS' | 'ADJUSTS' | 'MEASURES';
