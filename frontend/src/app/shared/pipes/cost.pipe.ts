0a85f6594342431b7a721ec8750b28ea459b1053
Isadora <isah3027@gmail.com>
2024-02-22 19:49:59 -0300

    refactor: remove missing console.log from pipe file

--- frontend/src/app/shared/pipes/cost.pipe.ts
+++ frontend/src/app/shared/pipes/cost.pipe.ts
@@ -7,7 +7,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CostPipe implements PipeTransform {
 
   transform(value: number): string {
-    console.log(value);
     const formattedValue = value.toFixed(2);
     return formattedValue;
   }
