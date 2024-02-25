8777ba17ab72aa4cc22bf960195ae3df40eefa66
Isadora <isah3027@gmail.com>
2024-02-25 16:33:12 -0300

    feat: create add-measure-dialog-component

--- frontend/src/app/features/measures/components/add-measure-dialog/add-measure-dialog.component.ts
+++ frontend/src/app/features/measures/components/add-measure-dialog/add-measure-dialog.component.ts
@@ -3,33 +3,30 @@ import {
   FormControl,
   FormGroup,
   Validators,
-  ReactiveFormsModule
+  ReactiveFormsModule,
 } from '@angular/forms';
 
 import { DialogModule } from 'primeng/dialog';
-import { InputNumberModule } from 'primeng/inputnumber';
 
 import { InputComponent } from '../../../../shared/components/input/input.component';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
-  selector: 'app-add-adjust-dialog',
+  selector: 'app-add-measure-dialog',
   standalone: true,
   imports: [
     ReactiveFormsModule,
     DialogModule,
     InputComponent,
     ButtonComponent,
-    InputNumberModule,
   ],
-  templateUrl: './add-adjust-dialog.component.html',
+  templateUrl: './add-measure-dialog.component.html',
   styleUrls: [
-    './add-adjust-dialog.component.css',
+    './add-measure-dialog.component.css',
     '/src/app/shared/styles/dialog.css',
-    '/src/app/shared/styles/input-number.css',
   ]
 })
-export class AddAdjustDialogComponent {
+export class AddMeasureDialogComponent {
   private _addFormGroup: FormGroup<AddFormGroupFields>;
 
   @Input()
@@ -38,15 +35,14 @@ export class AddAdjustDialogComponent {
   @Output("onDialogVisibilityChange")
   public onDialogVisibilityChangeEvent: EventEmitter<void>;
 
-  @Output('onSaveAdjust')
-  public onSaveAdjustEvent: EventEmitter<AddAdjustData>;
+  @Output('onSaveMeasure')
+  public onSaveMeasureEvent: EventEmitter<AddMeasureData>;
 
   constructor() {
     this._addFormGroup = new FormGroup<AddFormGroupFields>({
       name: new FormControl(null, [Validators.required]),
-      cost: new FormControl(null, [Validators.required]),
     });
-    this.onSaveAdjustEvent = new EventEmitter();
+    this.onSaveMeasureEvent = new EventEmitter();
     this.onDialogVisibilityChangeEvent = new EventEmitter();
 
     this.isDialogVisible = false;
@@ -60,20 +56,17 @@ export class AddAdjustDialogComponent {
     this.onDialogVisibilityChangeEvent.emit();
   }
 
-  onSaveAdjust() {
-    this.onSaveAdjustEvent.emit({
-      name: this._addFormGroup.value.name || null,
-      cost: this._addFormGroup.value.cost || null,
+  onSaveMeasure() {
+    this.onSaveMeasureEvent.emit({
+      name: this._addFormGroup.value.name || null
     });
   }
 }
 
-export interface AddAdjustData {
+export interface AddMeasureData {
   name: string | null;
-  cost: number | null;
 }
 
 interface AddFormGroupFields {
   name: FormControl<string | null>;
-  cost: FormControl<number | null>;
 }
--- frontend/src/app/features/measures/measures.component.ts
+++ frontend/src/app/features/measures/measures.component.ts
@@ -1,7 +1,8 @@
 import {
   MeasureService,
   MeasurePage,
-  QueryParams
+  QueryParams,
+  SaveMeasureDTO
 } from '../../services/measure.service';
 
 import { CommonModule } from '@angular/common';
@@ -9,10 +10,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 
 import { PaginatorModule, PaginatorState } from 'primeng/paginator';
+import { Message, MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { ButtonComponent } from '../../shared/components/button/button.component';
 
 import { AvailableFilters, FilterComponent } from './components/filter/filter.component';
+import { AddMeasureData, AddMeasureDialogComponent } from './components/add-measure-dialog/add-measure-dialog.component';
 
 @Component({
   selector: 'app-measures',
@@ -21,17 +26,24 @@ import { AvailableFilters, FilterComponent } from './components/filter/filter.co
     CommonModule,
     HeaderComponent,
     PaginatorModule,
+    ToastModule,
     FilterComponent,
+    ButtonComponent,
+    AddMeasureDialogComponent
   ],
+  providers: [MessageService],
   templateUrl: './measures.component.html',
   styleUrl: './measures.component.css'
 })
 export class MeasuresComponent implements OnInit {
   private measureService: MeasureService = inject(MeasureService);
+  private messageService: MessageService = inject(MessageService);
 
   private _activeFilters: AvailableFilters;
   private _measurePage?: MeasurePage
   private _currentPage: number;
+
+  public activeModal: AvailableModalsType | null;
 
   constructor() {
     this._activeFilters = {
@@ -39,6 +51,7 @@ export class MeasuresComponent implements OnInit {
       status: null
     };
     this._currentPage = 0;
+    this.activeModal = null;
   }
 
   ngOnInit(): void {
@@ -75,6 +88,10 @@ export class MeasuresComponent implements OnInit {
     return 0;
   }
 
+  showToast(message: Message) {
+    this.messageService.add(message);
+  }
+
   applyFilters(applyedFilter: AvailableFilters) {
     this._activeFilters = applyedFilter;
     this.fetchMeasures();
@@ -97,4 +114,52 @@ export class MeasuresComponent implements OnInit {
     })
   }
 
+  isModalVisible(type: AvailableModalsType) {
+    return this.activeModal !== null && this.activeModal === type
+      ? true
+      : false;
+  }
+
+  closeModal() {
+    this.activeModal = null;
+  }
+
+  showAddMeasureDialog() {
+    this.activeModal = 'ADD';
+  }
+
+  saveMeasure(measureData: AddMeasureData) {
+    const saveMeasure: SaveMeasureDTO = {
+      name: measureData.name,
+    };
+
+    this.measureService.addMeasure(saveMeasure).subscribe({
+      next: () => {
+        this.closeModal();
+        this.showToast({
+          severity: 'success',
+          summary: 'Sucesso',
+          detail: 'Medida cadastrada com sucesso',
+        });
+        this.fetchMeasures();
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+        this.showToast({
+          severity: 'error',
+          summary: 'Erro',
+          detail: error.error.details,
+        });
+      },
+    });
+  }
+
 }
+
+enum AvailableModals {
+  ADD,
+  UPDATE,
+  DELETE,
+}
+
+type AvailableModalsType = keyof typeof AvailableModals;
